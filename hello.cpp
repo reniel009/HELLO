c57b13ec422cb807760d8549e24868ecb7308b17
Oliquino <425003510@ntc.edu.ph>
2026-08-28 12:59:52 +0800

    Week 1: verified C++ environment

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // enables std::cout for output
+using namespace std;
+int main() {
+// A simple message to confirm compilation succeeded
+cout << "Environment verified. Ready for Data Structures." << endl;
+return 0; // 0 signals successful program exit
+}
